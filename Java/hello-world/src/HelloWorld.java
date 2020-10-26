de771a651e1cff8940ad1257586eb600fa5cbeb4
wagner maidson <wagner.rmaidson@gmail.com>
2020-10-26 19:54:27 -0300

    :tada: Initial commit for first java class!

---
+++ Java/hello-world/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * beginning of studies in java
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // this is a simple example in java to start with the right foot
+    }
+}
