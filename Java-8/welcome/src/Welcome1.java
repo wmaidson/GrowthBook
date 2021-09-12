4c5f0efd99d9bcadd1fa2108e699fcca63732cdc
wagner maidson <wagner.rmaidson@gmail.com>
2021-09-12 14:40:19 -0300

    adjust files in Java-8

---
+++ Java-8/welcome/src/Welcome1.java
@@ -0,0 +1,11 @@
+// Figura 2.1 Welcome1.java
+// Programa de impressão de texto.
+
+public class Welcome1 {
+
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java Programming!");
+    } // fim do método main
+} // fim da classe Welcome1
---
+++ Java-8/welcome/src/Welcome2.java
@@ -0,0 +1,13 @@
+// Figura 2.3: Welcome2.java
+// Imprimindo uma linha de texto com múltiplas instruções.
+
+public class Welcome2 {
+
+    // método main inicia a excecução do aplicativo Java
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to ");
+        System.out.println("Java Programming!");
+
+    } // fim do método maim
+} // fim da classe Welcome2
