5ff560bfdd9d2f96e648351d1d99b82cc122a01f
wagner maidson <wagner.rmaidson@gmail.com>
2021-09-17 22:46:43 -0300

    Create Class for test

---
+++ Java-8/2-introduction_to_java application/src/Welcome3.java
@@ -0,0 +1,10 @@
+// Figura 2.5: Welcome3.java
+// Imprimindo múltiplas linhas de texto com uma única instrução
+
+public class Welcome3 {
+
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        System.out.print("Welcome\nto\nJava\nProgramming!");
+    } // fim do método main
+} // fim da classe welcome3
