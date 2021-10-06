f2529c08c995e2418d8620fcf5a7ee6a329bf159
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-05 22:10:42 -0300

    move java-8 into java repository

---
+++ Java/Java-8/2-introduction-to-java-application/src/Welcome4.java
@@ -0,0 +1,11 @@
+// Figura 2.6: Welcome4.java
+// Exibindo múltiplas linhas com o método System.out.printf.
+
+public class Welcome4 {
+
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n",
+                "Welcome to", "Java Programming!");
+    } // fim do método main
+} // fim da classe welcome4
---
+++ Java/Java-8/3-introduction-to-class-objects-methods-and-strings/README.md
@@ -0,0 +1,1 @@
+## Introdução a classes, objetos, métodos e strings
