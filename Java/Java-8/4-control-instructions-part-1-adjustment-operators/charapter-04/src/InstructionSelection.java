7462c0a1b7f66e027245e72e57adf93362152231
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-18 22:54:19 -0300

    create simple if instruction selection

---
+++ Java/Java-8/4-control-instructions-part-1-adjustment-operators/charapter-04/src/InstructionSelection.java
@@ -0,0 +1,10 @@
+public class InstructionSelection {
+
+    public static void main(String[] args) {
+
+        int studentGrade = 61;
+
+        if (studentGrade >= 60)
+            System.out.println("Passed");
+    }
+}
