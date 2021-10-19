5ec9d1c1d6681d605a64bfbd11b4bdf079e9a653
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-18 22:55:41 -0300

    create simple if else double instruction selection

---
+++ Java/Java-8/4-control-instructions-part-1-adjustment-operators/charapter-04/src/DoubleInstructionSelection.java
@@ -0,0 +1,12 @@
+public class DoubleInstructionSelection {
+
+    public static void main(String[] args) {
+
+        int grade = 59;
+
+        if (grade >=60)
+            System.out.println("Passed");
+        else
+            System.out.println("Failed");
+    }
+}
