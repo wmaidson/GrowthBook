72719a1e4e5d63053335010536dc7a3d7c29ec85
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-18 23:26:47 -0300

    else for two instruction

---
+++ Java/Java-8/4-control-instructions-part-1-adjustment-operators/charapter-04/src/Blocks.java
@@ -0,0 +1,15 @@
+public class Blocks {
+
+    public static void main(String[] args) {
+
+        int grade = 59;
+
+        if (grade >= 60)
+            System.out.println("Passed");
+        else
+        {
+            System.out.println("Failed");
+            System.out.println("You must take this course again.");
+        }
+    }
+}
