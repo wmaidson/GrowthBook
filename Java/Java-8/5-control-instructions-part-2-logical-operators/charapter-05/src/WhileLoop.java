fb261c1857e28c81707470b781f406ef24d2851e
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-20 23:28:03 -0300

    Loop continuation condition

---
+++ Java/Java-8/5-control-instructions-part-2-logical-operators/charapter-05/src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (++counter <= 10) // condição de continuação do loop
+            System.out.printf("%d ", counter);
+    }
+}
