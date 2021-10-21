6d72c4a90675d55e1914ec322fdd0db90cd3d4a0
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-20 23:45:20 -0300

    Do...while repetition statement.

---
+++ Java/Java-8/5-control-instructions-part-2-logical-operators/charapter-05/src/DoWhileTest.java
@@ -0,0 +1,12 @@
+public class DoWhileTest {
+
+    public static void main(String[] args) {
+
+        int counter = 1;
+
+        do {
+            System.out.printf("%d ", counter);
+            ++counter;
+        } while (counter <= 10); // fim da instrução do...while
+    }
+}
