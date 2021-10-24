4379f86e2958e4a125e46845fba21bedf1dbc51b
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-23 21:05:16 -0300

    Equality operators and relational operators

---
+++ Java/Java-8/2-introduction-to-java-application/src/Comparison.java
@@ -0,0 +1,41 @@
+// Figura 2.15: Comparison.java
+// Compara inteiros utilizando instruções if, operadores relacionais
+// e operadores de igualdade.
+import java.util.Scanner; // programa utiliza a classe Scanner
+
+public class Comparison {
+
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+
+        // cria Scanner para obter entrada a partir da linha de comando
+        Scanner input = new Scanner(System.in);
+
+        int number1; // primeiro número a comparar
+        int number2; // segundo número a comparar
+
+        System.out.print("Enter first integer: "); // prompt
+        number1 = input.nextInt(); // lê o primeiro número fornecido pelo usuário
+
+        System.out.print("Enter second integer: "); // prompt
+        number2 = input.nextInt(); // lê o segundo número fornecido pelo usuário
+
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
+
+        if (number1 != number2)
+            System.out.printf("%d != %d%n", number1, number2);
+
+        if (number1 < number2)
+            System.out.printf("%d < %d%n", number1, number2);
+
+        if (number1 > number2)
+            System.out.printf("%d > %d%n", number1, number2);
+
+        if (number1 <= number2)
+            System.out.printf("%d <= %d%n", number1, number2);
+
+        if (number1 >= number2)
+            System.out.printf("%d >= %d%n", number1, number2);
+    }
+}
