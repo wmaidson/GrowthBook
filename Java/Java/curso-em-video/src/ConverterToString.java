ecf7b945debab41ca0e3afe9547793d1fdef1d3b
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:37:14 -0300

    converter para String

---
+++ Java/Java/curso-em-video/src/ConverterToString.java
@@ -0,0 +1,9 @@
+public class ConverterToString {
+
+    public static void main(String[] args) {
+
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+    }
+}
