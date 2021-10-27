650f6d23a408dfd996727570abe62c6ae193fc02
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:33:38 -0300

    arredonda para baixo

---
+++ Java/Java/curso-em-video/src/ArredondaParaBaixo.java
@@ -0,0 +1,8 @@
+
+public class ArredondaParaBaixo {
+    public static void main(String[] args) {
+        float v = 8.9f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);
+    }
+}
