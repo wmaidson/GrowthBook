c6cebcd7f86337b9425d944ea9ccbc59b712439e
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:32:35 -0300

    arredonda aritmeticamente

---
+++ Java/Java/curso-em-video/src/ArredondaAritmeticamente.java
@@ -0,0 +1,8 @@
+
+public class ArredondaAritmeticamente {
+    public static void main(String[] args) {
+        float v = 8.5f;
+        int ar = (int) Math.round(v);
+        System.out.println(ar);
+    }
+}
