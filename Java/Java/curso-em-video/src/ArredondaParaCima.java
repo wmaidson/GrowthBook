19c36a9b06bbe7ab5f41b2bf5950dd79e403e60f
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:34:20 -0300

    arredonda para cima

---
+++ Java/Java/curso-em-video/src/ArredondaParaCima.java
@@ -0,0 +1,8 @@
+
+public class ArredondaParaCima {
+    public static void main(String[] args) {
+        float v = 8.9f;
+        int ar = (int) Math.ceil(v);
+        System.out.println(ar);
+    }
+}
