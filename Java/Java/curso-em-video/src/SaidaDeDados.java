dd963399f83601391084f7645fbd059f69163eab
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:46:39 -0300

    testes de output

---
+++ Java/Java/curso-em-video/src/SaidaDeDados.java
@@ -0,0 +1,11 @@
+public class SaidaDeDados {
+
+    public static void main(String[] args) {
+
+        float nota = 8.5f;
+        String nome = "Nome";
+        System.out.println("A nota é " + nota);
+        System.out.printf("A nota de %s é %.2f\n", nome, nota);
+        System.out.format("A nota de %s é %.2f\n", nome, nota);
+    }
+}
