ad1c06dcd5a2a63a1da17363a9fb3557a5fbc813
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:35:59 -0300

    converter para flooat

---
+++ Java/Java/curso-em-video/src/ConverterParseFloat.java
@@ -0,0 +1,9 @@
+public class ConverterParseFloat {
+
+    public static void main(String[] args) {
+
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor);
+        System.out.println(idade);
+    }
+}
