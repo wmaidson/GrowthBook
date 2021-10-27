43b6e0bed6707324063de0383955de1bbd96e37f
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:46:09 -0300

    Ver a resolucao da tela com Dimension get screen size

---
+++ Java/Java/curso-em-video/src/ResolucaoTela.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class ResolucaoTela {
+
+    public static void main(String[] args) {
+
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println(size);
+    }
+}
