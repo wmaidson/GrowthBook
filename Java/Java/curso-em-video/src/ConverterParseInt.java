f41d5a9856e7b66cae0439f86c2adeb8cb3360b7
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-26 23:36:43 -0300

    converter para int

---
+++ Java/Java/curso-em-video/src/ConverterParseInt.java
@@ -0,0 +1,8 @@
+public class ConverterParseInt {
+
+    public static void main(String[] args) {
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
+    }
+}
