bddd5005a46030680c01c79ffac854ecec33a78f
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-28 23:46:38 -0300

    comparação de objetos e conteúdo de objestos

---
+++ Java/Java/curso-em-video/src/TestaComparacao.java
@@ -0,0 +1,12 @@
+public class TestaComparacao {
+
+    public static void main(String[] args) {
+
+        String parte1 = "Curso";
+        String parte2 = "Video";
+        String parte3 = parte1 + parte2;
+        String parte4 = "CursoVideo";
+        System.out.println(parte3 == parte4);
+        System.out.println(parte3.equals(parte4));
+    }
+}
