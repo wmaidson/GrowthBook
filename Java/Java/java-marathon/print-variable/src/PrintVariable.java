41f72ee4956746cd309b17fe87e0f97957ee7e09
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-20 23:49:24 -0300

    DevDojo

---
+++ Java/Java/java-marathon/print-variable/src/PrintVariable.java
@@ -0,0 +1,15 @@
+public class PrintVariable {
+
+    /***
+     *
+     * @param args method input parameter
+     */
+
+    public static void main(String[] args) {
+
+        String name = "wagner";
+
+        System.out.println(name.length());
+
+    }
+}
