81e7e0d8630ba24c436b6ca9c90ce0115df06119
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-22 21:33:02 -0300

    1 relacionais

---
+++ Java/Java/DevDojo/16/src/Relacionais.java
@@ -0,0 +1,26 @@
+
+
+public class Relacionais {
+
+    public static void main(String[] args) {
+
+        int sum1 = 10;
+        int sum2  = 20;
+        double result = sum1 / (double) sum2;
+
+        System.out.println(result);
+
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        boolean comparaMaiorQueVinte = 10 > 20;
+        boolean comparaMenorQueVinte = 10 < 20;
+        boolean igual = 10 == 20;
+        boolean diferente = 10 != 20;
+        System.out.println(comparaMaiorQueVinte);
+        System.out.println(comparaMenorQueVinte);
+        System.out.println(igual);
+        System.out.println(diferente);
+
+    }   
+}
