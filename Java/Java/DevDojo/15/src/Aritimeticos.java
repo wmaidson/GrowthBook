78ffc80d8612421a163d5cf58c63a7b7a7110719
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-22 21:32:23 -0300

    15 aritimeticos

---
+++ Java/Java/DevDojo/15/src/Aritimeticos.java
@@ -0,0 +1,13 @@
+
+
+public class Aritimeticos {
+
+    public static void main(String[] args) {
+
+        int sum1 = 10;
+        int sum2  = 20;
+        double result = sum1 / (double) sum2;
+
+        System.out.println(result);
+    }   
+}
