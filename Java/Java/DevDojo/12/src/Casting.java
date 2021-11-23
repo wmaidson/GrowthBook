bc8946d125b0639dc18fe0a25cbfc03a57c55fe5
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-22 21:29:17 -0300

    12 casting

---
+++ Java/Java/DevDojo/12/src/Casting.java
@@ -0,0 +1,15 @@
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        int idade = (int) 10000000000L;
+        long numeroGrande = 1000L;
+        double salarioDoble = 2000D;
+        float salarioFloat = (float)2500D;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+    }
+}
