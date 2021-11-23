395f7513a33bd31ad4f756640af6d03ed9328d17
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-22 21:28:05 -0300

    11 tipos primitivos

---
+++ Java/Java/DevDojo/11/src/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        int idade;
+        long numeroGrande = 1000;
+        double salarioDoble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+    }
+}
