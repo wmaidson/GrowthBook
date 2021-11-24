e4ca6f949d8e0b6519a3448267698c5aa40f7c96
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-23 23:21:02 -0300

    teste de JOptionPane

---
+++ Java/Java/curso-em-video/src/Painel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Painel {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Ola Mundo!", "Boas vindas", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "mensagem!", "Aviso", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null, "mensagem!", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+}
