08ba3992a7dd1bc07926060b3219bcb5da49b46e
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 19:52:19 -0300

    repeticao com numero negativo

---
+++ Java/Java/curso-em-video/src/RepeticaoForMenosNegativo.java
@@ -0,0 +1,9 @@
+public class RepeticaoForMenosNegativo {
+
+    public static void main(String[] args) {
+
+        for (int i = 5; i >= -5; i--) {
+            System.out.println(i);
+        }
+    }
+}
