b2729d12282236bbd9b38ceccbcd7f49db5ba8d3
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-23 23:49:27 -0300

    calculo de porcentagem

---
+++ Programming-logic/DevDojo/src/RealizandoVariaveis.java
@@ -0,0 +1,10 @@
+public class RealizandoVariaveis {
+
+    public static void main(String[] args) {
+
+        double salario = 5000;
+        double resultado = salario * 0.3;
+        System.out.println(resultado);
+
+    }
+}
