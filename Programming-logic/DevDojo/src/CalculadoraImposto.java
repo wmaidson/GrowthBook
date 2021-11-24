8e1a0483d24a9589578e2074420878b23830d373
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-23 23:49:08 -0300

    calculo de imposto

---
+++ Programming-logic/DevDojo/src/CalculadoraImposto.java
@@ -0,0 +1,10 @@
+public class CalculadoraImposto {
+
+    public static void main(String[] args) {
+        double salario = 2500.00F;
+        float porcentagem = 30L;
+        double procentagemDoSalario = salario * (porcentagem / 100);
+        System.out.println(procentagemDoSalario);
+
+    }
+}
