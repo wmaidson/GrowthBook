a05a61211d3f0f79f8bd0967aeb1ce323c09e7f6
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-23 23:21:42 -0300

    teste do repeticao

---
+++ Java/Java/curso-em-video/src/Repeticao.java
@@ -0,0 +1,11 @@
+public class Repeticao {
+
+    public static void main(String[] args) {
+
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++;
+        } while (cc < 4);
+    }
+}
