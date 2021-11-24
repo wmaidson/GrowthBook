d935fa3657ef7cc26898b6e8a5504c5d1b69d07a
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 19:53:03 -0300

    repeticao com for e multiplo de dez

---
+++ Java/Java/curso-em-video/src/RepeticaoForMultiplicacaoDez.java
@@ -0,0 +1,8 @@
+public class RepeticaoForMultiplicacaoDez {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 10) {
+            System.out.println(i);
+        }
+    }
+}
