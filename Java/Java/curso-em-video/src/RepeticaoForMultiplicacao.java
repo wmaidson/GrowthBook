67f5f7fdae7b6ebe01a965565adc1589a16cd9a3
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 19:52:32 -0300

    repeticao com multiplicacao

---
+++ Java/Java/curso-em-video/src/RepeticaoForMultiplicacao.java
@@ -0,0 +1,8 @@
+public class RepeticaoForMultiplicacao {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 32; i *= 2) {
+            System.out.println(i);
+        }
+    }
+}
