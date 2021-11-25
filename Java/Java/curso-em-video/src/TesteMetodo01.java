cda1244aa23a4a697f02a6b83689f572384e76bb
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 23:47:18 -0300

    teste de metodo

---
+++ Java/Java/curso-em-video/src/TesteMetodo01.java
@@ -0,0 +1,15 @@
+public class TesteMetodo01 {
+
+    static void soma(int a, int b) {
+
+        int s = a + b;
+        System.out.println("A soma e " + s);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Comecou o programa");
+        soma(5, 2);
+
+    }
+}
