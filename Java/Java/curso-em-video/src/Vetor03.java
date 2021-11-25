a2148ea87a701fa01188fa477d7944696db9bcc0
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 22:25:04 -0300

    teste de vetor03

---
+++ Java/Java/curso-em-video/src/Vetor03.java
@@ -0,0 +1,11 @@
+public class Vetor03 {
+
+    public static void main(String[] args) {
+
+        double[] v = {3.5, 2.75, 4.50, 6.25};
+
+        for (double value: v) {
+            System.out.println(value + " ");
+        }
+    }
+}
