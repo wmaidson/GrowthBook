4870c9b86d9a6d33cd16cd74518af1326defc919
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 21:18:14 -0300

    teste vetor 01

---
+++ Java/Java/curso-em-video/src/Vetor01.java
@@ -0,0 +1,12 @@
+public class Vetor01 {
+
+    public static void main(String[] args) {
+        
+        int[] n = {3, 5, 7, 9};
+
+        System.out.println("Total de casas de N " + n.length);
+        for(int  c =0; c <= 5; c++) {
+            System.out.println("Na posicao:  " + c + " temos o valor: " + n[c]);
+        }
+    }
+}
