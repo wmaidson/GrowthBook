ea85dcd3916986cc9298152642cb86058fe0ef74
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2021-11-25 17:47:41 -0300

    teste valores

---
+++ Java/Java/curso-em-video/src/TesteValores.java
@@ -0,0 +1,13 @@
+public class TesteValores {
+
+  public static void main(String[] args) {
+    int x, y;
+    double z;
+
+    x = 5;
+    y = 2;
+    z = x / y;
+    System.out.print(z);
+
+  }
+}
