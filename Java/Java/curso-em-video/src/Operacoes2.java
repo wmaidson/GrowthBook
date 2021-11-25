6f8aaf589210ca1644830ffe208b1b31aa8cac01
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 23:48:39 -0300

    teste operacoes privada

---
+++ Java/Java/curso-em-video/src/Operacoes2.java
@@ -0,0 +1,11 @@
+public class Operacoes2 {
+
+    private static String contador (int i, int f) {
+        String s = "";
+        for (int c = i; c <= f; c++) {
+            s += c + " ";
+        }
+        return s;
+
+    }
+}
