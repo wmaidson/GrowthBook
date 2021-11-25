33ab0749904822d0eb31d8b158c423f7d935bbeb
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2021-11-25 17:46:25 -0300

    teste nome vazio

---
+++ Java/Java/curso-em-video/src/TesteEmptyName.java
@@ -0,0 +1,15 @@
+public class TesteEmptyName {
+  public static void main(String[] args) {
+
+    String nome = "João";
+    imprimeNome("Empty");
+  }
+  public static void imprimeNome(String nome) {
+    if(!nome.isEmpty()){
+      System.out.println("Tudo bem " + nome + "?");
+    } else {
+      System.out.println("O nome é " + nome + "?");
+    }
+
+  }
+}
