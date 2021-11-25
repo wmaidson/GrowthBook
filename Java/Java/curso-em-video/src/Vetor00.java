dc87fb4723db399492d6be8eb1085f03227fcda6
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 21:18:06 -0300

    teste vetor 00

---
+++ Java/Java/curso-em-video/src/Vetor00.java
@@ -0,0 +1,15 @@
+public class Vetor00 {
+
+    public static void main(String[] args) {
+        
+        int n[] = new int[4];
+
+        n[0] = 3;
+        n[1] = 5;
+        n[2] = 7;
+        n[3] = 9;
+
+        System.out.println(n[0]);
+        
+    }
+}
