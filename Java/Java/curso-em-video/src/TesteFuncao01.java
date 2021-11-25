546533cb02c0ca601b67cc3af623c71577f4a9e9
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-24 23:47:44 -0300

    teste de funcao

---
+++ Java/Java/curso-em-video/src/TesteFuncao01.java
@@ -0,0 +1,15 @@
+public class TesteFuncao01 {
+
+    static int soma(int a, int b) {
+
+        int s = a + b;
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Comecou o programa");
+        int sm = soma(5, 2);
+        System.out.println("A soma vale " + sm);
+    }
+}
---
+++ Java/Java/curso-em-video/src/TesteFuncao02.java
@@ -0,0 +1,7 @@
+public class TesteFuncao02 {
+
+    public static void main(String[] args) {
+        System.out.println("contagem:");
+        System.out.println(Operacoes.contador(1, 5));
+    }
+}
