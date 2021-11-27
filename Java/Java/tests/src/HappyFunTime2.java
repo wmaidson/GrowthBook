a039721a87da0c356c7003ac28749b0e49e7113d
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-27 02:03:19 -0300

    For Loop - Com Array de String com 3 elementos

---
+++ Java/Java/tests/src/HappyFunTime2.java
@@ -0,0 +1,13 @@
+public class HappyFunTime2 {
+
+    public static void main(String[] args) {
+
+      String[] cars = {"BMW M2", "Veloster M", "GTI"};
+
+      for(int i = 0; i < cars.length; i++) {
+          System.out.println(i);
+          System.out.println(cars);
+          System.out.println(cars[i]);
+      }
+    }
+}
