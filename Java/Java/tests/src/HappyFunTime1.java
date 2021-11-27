eb1aac90c2ee6ee5a3700e56a8996ee905bd018b
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-27 02:02:50 -0300

    For Loop - Quando você souber exatamente quantas vezes deseja percorrer um bloco de código

---
+++ Java/Java/tests/src/HappyFunTime1.java
@@ -0,0 +1,15 @@
+/**
+ * A instrução 1 é executada (uma vez) antes da execução do bloco de código.
+ * A instrução 2 define a condição para a execução do bloco de código.
+ * A instrução 3 é executada (todas as vezes) após a execução do bloco de código.
+ */
+
+public class HappyFunTime1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
