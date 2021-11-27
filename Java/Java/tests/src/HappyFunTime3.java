c65fbb9ca6388df441422f47c5309ad87e2a821a
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-27 02:03:29 -0300

    For Each - Com Array de String com 3 elementos

---
+++ Java/Java/tests/src/HappyFunTime3.java
@@ -0,0 +1,11 @@
+public class HappyFunTime3 {
+
+    public static void main(String[] args) {
+
+      String[] cars = {"BMW M2", "Veloster M", "GTI"};
+
+      for(String car: cars) {
+          System.out.println(car);
+      }
+    }
+}
