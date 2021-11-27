4136af31a72658e832c9f320dca8d3a1ca63d807
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-27 02:03:50 -0300

    For Each - Com Array de char com 3 elementos

---
+++ Java/Java/tests/src/HappyFunTime4.java
@@ -0,0 +1,10 @@
+public class HappyFunTime4 {
+
+    public static void main(String[] args) {
+
+      char[] cars = {'a', 'b', 'c'};
+      for(char car: cars) {
+          System.out.println(car);
+      }
+    }
+}
