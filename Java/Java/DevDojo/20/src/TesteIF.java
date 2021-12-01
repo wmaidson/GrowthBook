a288c7bbc3f7fb46abdcd90c258178fa1bd251a1
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-30 23:46:03 -0300

    if

---
+++ Java/Java/DevDojo/20/src/TesteIF.java
@@ -0,0 +1,13 @@
+
+
+public class LogicosOR  {
+
+    public static void main(String[] args) {
+
+        if(true) {
+            System.out.println("verdadeiro");
+        }
+        
+
+    }   
+}
