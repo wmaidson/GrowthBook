3ab6068127e54befed1f5e7f97b17f74d0c132ce
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-30 23:45:47 -0300

    Atribuição

---
+++ Java/Java/DevDojo/19/src/Atribuicao.java
@@ -0,0 +1,13 @@
+
+
+public class Atribuicao  {
+
+    public static void main(String[] args) {
+
+       double bonus = 1800;
+       bonus +=+ 1000;
+
+       System.out.println(bonus);
+
+    }   
+}
