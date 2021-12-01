60c76d80a2a62668916a143dcf14b737574630cf
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-30 23:45:29 -0300

    contador

---
+++ Java/Java/DevDojo/19/src/Contador.java
@@ -0,0 +1,14 @@
+
+
+public class Atribuicao  {
+
+    public static void main(String[] args) {
+
+      int contador = 0;
+      contador += 1;
+      contador++;
+
+       System.out.println(contador);
+
+    }   
+}
