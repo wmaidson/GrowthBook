5e35073d64cb71421fc2e5148463b1acee7cd2f6
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-02 21:51:04 -0300

    instânciar um objeto

---
+++ Java/Java/POO/src/NomeDaClasse.java
@@ -0,0 +1,13 @@
+public class NomeDaClasse {
+
+    public static void main(String[] args) {
+
+        NomeDaClasse nomeDoObjeto;
+        nomeDoObjeto = new NomeDaClasse();
+
+        // As duas itruções podem ser combinada em uma só
+
+        NomeDaClasse nomeDoObjeto2 = new NomeDaClasse();
+
+    }
+}
