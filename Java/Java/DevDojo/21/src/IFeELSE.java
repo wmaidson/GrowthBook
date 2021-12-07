fa38911a7fef00a009ecaa3451fd5480ae63d017
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-06 22:07:43 -0300

    ELSE IF

---
+++ Java/Java/DevDojo/21/src/IFeELSE.java
@@ -0,0 +1,13 @@
+public class IFeELSE  {
+
+    int idade = 15;
+
+    public static void main(String[] args) {
+
+        if(idade > 15) {
+            System.out.println("Autoriza");
+        } else {
+            System.out.println("Nao autoriza");
+        }
+    }   
+}
