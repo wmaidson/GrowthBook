962c34d0d24d27b706513aeefba113ce246dff67
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-06 22:08:08 -0300

    Operador ternário

---
+++ Java/Java/DevDojo/22/src/EstruturaCondicionais.java
@@ -0,0 +1,17 @@
+public class EstruturaCondicionais {
+
+    public static void main(String[] args) {
+
+        double salario = 3000;
+        String messageD = "vou doar";
+        String messageN = "Nao doar";
+        String result;
+
+        String result = salario > 5000 messageD : messageN;
+
+
+        System.out.println(result);
+
+
+    }
+}
