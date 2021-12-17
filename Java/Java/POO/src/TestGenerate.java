5f1e293cd83ff914b74e7a48836fcec44ae7f9a1
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-17 00:37:02 -0300

    teste generate

---
+++ Java/Java/POO/src/TestGenerate.java
@@ -0,0 +1,10 @@
+public class TestGenerate {
+
+    public int valor1 = 10;
+    private int valor2 = 20;
+     protected int valor3 = 20;
+
+    private static void main(String[] args) {
+
+    }
+}
