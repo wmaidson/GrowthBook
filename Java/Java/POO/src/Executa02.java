73901743589818262e7446cfaf07b413f9fad490
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-17 00:36:52 -0300

    main teste para classe teste 03

---
+++ Java/Java/POO/src/Executa02.java
@@ -0,0 +1,12 @@
+public class Executa02 {
+
+    public static void main(String[] args) {
+        Caneta02 c1 = new Caneta02("NIC", "Amarelo", 0.4f);
+        c1.status();
+
+        System.out.println("************");
+
+        Caneta02 c2 = new Caneta02("AAA", "Preto", 1.5f);
+        c2.status();
+    }
+}
