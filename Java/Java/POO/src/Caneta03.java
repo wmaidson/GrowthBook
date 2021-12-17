2a4a58a883d3182e6d5243b97638eddc0cfbcb38
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-17 00:36:15 -0300

    classe de teste 03

---
+++ Java/Java/POO/src/Caneta03.java
@@ -0,0 +1,13 @@
+public class Caneta03 {
+
+    private String cor;
+    private String modelo;
+    private float ponta;
+    private boolean tampada;
+
+    public Caneta03(String cor, String modelo, float ponta) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.ponta = ponta;
+    }
+}
