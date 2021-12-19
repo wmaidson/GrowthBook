00c7b70371f63b464d2c97fab3baef963e4e2f7c
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-19 20:46:20 -0300

    Composition demonstration.

---
+++ Java/Java-8/8-Classes-and-Objects-An-Exam-deeper/chapter-08/src/EmployeeTest.java
@@ -0,0 +1,14 @@
+// Figura 8.9: EmployeeTest.java
+// Demonstração de composição.
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+
+        Date birth = new Date(7, 24, 1949);
+        Date hire = new Date(3, 12, 1988);
+        Employee employee = new Employee("Bob", "Blue", birth, hire);
+
+        System.out.println(employee);
+    }
+}
