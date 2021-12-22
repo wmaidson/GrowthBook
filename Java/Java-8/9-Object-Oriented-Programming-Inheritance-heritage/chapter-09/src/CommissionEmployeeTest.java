024b73e7a3f63d0ebf5aa8a20e7c5c5db704d1b0
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-21 23:34:33 -0300

    Test program for the CommissionEmployee class.

---
+++ Java/Java-8/9-Object-Oriented-Programming-Inheritance-heritage/chapter-09/src/CommissionEmployeeTest.java
@@ -0,0 +1,31 @@
+// Figura 9.5: CommissionEmployeeTest.java
+// Programa de teste da classe CommissionEmployee.
+
+public class CommissionEmployeeTest {
+
+    public static void main(String[] args) {
+        // instancia o objeto CommissionEmployee
+        CommissionEmployee employee = new CommissionEmployee(
+                "Sue", "Jones", "222-22-2222", 10000, .06);
+
+        // obtém os dados de empregado comissionado
+        System.out.println(
+                "Employee information obtained by get methods:");
+        System.out.printf("%n%s %s%n", "First name is",
+                employee.getFirstName());
+        System.out.printf("%s %s%n", "Last name is",
+                employee.getLastName());
+        System.out.printf("%s %s%n", "Social security number is",
+                employee.getSocialSecurityNumber());
+        System.out.printf("%s %.2f%n", "Gross sales is",
+                employee.getGrossSales());
+        System.out.printf("%s %.2f%n", "Commission rate is",
+                employee.getCommissionRate());
+
+        employee.setGrossSales(5000);
+        employee.setCommissionRate(.1);
+
+        System.out.printf("%n%s:%n%nï%sÿ%n",
+                "Updated employee information obtained by toString", employee);
+    }
+}
