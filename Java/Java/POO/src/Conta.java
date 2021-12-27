451735370585801e244bc6e96c9036fd5f00db92
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-26 22:11:44 -0300

    criado entidade conta

---
+++ Java/Java/POO/src/Conta.java
@@ -0,0 +1,11 @@
+public class Conta {
+
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+        p1.estadoAtual();
+
+    }
+}
