2287baff3b743ab91e6fbd2a65835d37ac42a68e
wagner maidson <wagner.rmaidson@gmail.com>
2021-12-27 22:15:53 -0300

    main classe

---
+++ Java/Java/Lista-encadeadas/lista/src/ListaEncadeadaTeste.java
@@ -0,0 +1,9 @@
+public class ListaEncadeadaTeste {
+
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
+}
