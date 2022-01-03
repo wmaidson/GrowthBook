c473219383a9dd3e1851c441db68cce9d38b545c
wagner maidson <wagner.rmaidson@gmail.com>
2022-01-02 21:32:53 -0300

    criado lutador

--- Java/Java/POO/src/UltraEmojiCombat.java
+++ Java/Java/POO/src/UltraEmojiCombat.java
@@ -1,5 +1,10 @@
 public class UltraEmojiCombat {
     public static void main(String[] args) {
 
+        Lutador l1 = new Lutador("Pretty Boy", "Franca", 31, 1.75f,
+                68.9f, 11, 2, 1);
+        l1.apresentar();
+        l1.status();
+
     }
 }
