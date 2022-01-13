ebe1dc33af7546ab9c5647d4662eb00d883eb339
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2022-01-13 13:38:52 -0300

    script de comparação de string

---
+++ JavaScript/tests/scripts/comparando_strings.js
@@ -0,0 +1,8 @@
+var a = "a";
+var b = "b";
+if (a < b) // verdadeiro
+  print(a + " é menor que " + b);
+else if (a > b)
+  print(a + " é maior que " + b);
+else
+  print(a + " e " + b + " são iguais.");
