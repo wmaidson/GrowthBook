fb883953529c39b983f93ca6f94af83e73d68e18
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2022-01-14 10:35:40 -0300

    localizar valores em um array

---
+++ JavaScript/tests/scripts/test_indexOf.js
@@ -0,0 +1,8 @@
+// O exemplo seguinte usa indexOf() para localizar valores em um array
+
+var array = [2, 5, 9];
+array.indexOf(2);     // 0
+array.indexOf(7);     // -1
+array.indexOf(9, 2);  // 2
+array.indexOf(2, -1); // -1
+array.indexOf(2, -3); // 0
