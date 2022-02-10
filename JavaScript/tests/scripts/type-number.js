d1cdc673fd7b517ee6516e5064a8cf92dd8ce086
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2022-02-10 15:07:23 -0300

    type-number

---
+++ JavaScript/tests/scripts/type-number.js
@@ -0,0 +1,46 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+
+console.log("primeiroNumero ", primeiroNumero);
+console.log("segundoNumero ", segundoNumero);
+console.log("operacaoMatematica ", operacaoMatematica);
+
+console.log("------------------")
+
+const numeroPontoFlutante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutante;
+
+console.log(novaOperacao)
+
+console.log("------------------")
+
+// NaN -> Not A Number (não é um número)
+
+const texto = "text"
+
+console.log(texto * primeiroNumero)
+
+console.log("------------------")
+
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+console.log("------------------")
+
+var a1 = 0
+var b1 = 0
+console.log(a1/b1) // NaN
+
+
+
