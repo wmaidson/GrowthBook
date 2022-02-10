14f660ddd0d55f1803e26e341165943a43b629a4
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2022-02-10 16:25:06 -0300

    length

---
+++ JavaScript/tests/scripts/length.js
@@ -0,0 +1,8 @@
+var x = 'Mozilla';
+var empty = '';
+
+console.log('Mozilla possui ' + x.length + ' unidades de código de comprimento');
+/* "Mozilla possui 7 unidades de código de comprimento" */
+
+console.log('A string vazia possui um comprimento de ' + empty.length);
+/* "A string vazia possui um comprimento de 0" */
