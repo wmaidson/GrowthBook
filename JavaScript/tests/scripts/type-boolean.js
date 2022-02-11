112689b7e4450d5ac4e05d29d2f09ad43d1d1ad4
Wagner Maidson <49798275+wmaidson@users.noreply.github.com>
2022-02-11 09:48:51 -0300

    type boolean

---
+++ JavaScript/tests/scripts/type-boolean.js
@@ -0,0 +1,33 @@
+// boolean
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+let cadastroAtivo = true;
+
+const texto1 = "A";
+const texto2 = "a";
+
+console.log(primeiroNumero === segundoNumero);
+
+
+console.log("------------------")
+
+
+console.log(primeiroNumero === primeiroNumero);
+
+
+console.log("------------------")
+
+
+console.log(primeiroNumero != primeiroNumero);
+
+
+console.log("------------------")
+
+
+console.log("cadastroAtivo", cadastroAtivo);
+
+
+
+console.log("Comparar texto", texto1 === texto2);
