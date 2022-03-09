4204d67560c5dadef49748038a24f40c1063f974
wagner maidson <wagner.rmaidson@gmail.com>
2022-03-09 20:22:16 -0300

    remove c files

--- C/0003/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// imprimindo em uma linha com duas intruções printf
-int main() {
-    printf("Bem-vindo ");
-    printf("a C!\n");
-    return 0;
-}
--- C/0004/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-// Imprimindo várias linhas com uma única instrução printf
-int main() {
-    printf("Bem-vindo\na\nC!");
-    return 0;
-}
